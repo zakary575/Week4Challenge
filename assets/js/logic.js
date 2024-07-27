577e6c289dd2c6987023355140e400978f031450
Zakary Johnson <zakary.johnson575@gmail.com>
2024-07-26 20:33:39 -0500

    add code from lesson 11 for the light mode dark mode

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+const themeSwitcher = document.querySelector('#theme-switcher');
+const container = document.querySelector('.container');
+
+let mode = 'dark'
+
+themeSwitcher.addEventListener('click', function () {
+    // If mode is dark, apply light background
+    if (mode === 'dark') {
+      mode = 'light';
+      container.setAttribute('class', 'light');
+    }
+    // If mode is light, apply dark background
+    else {
+      mode = 'dark';
+      container.setAttribute('class', 'dark');
+    }
+  });
