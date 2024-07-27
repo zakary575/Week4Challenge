869ad5522499a725c21119f5c8e6eb50d53c046b
Zakary Johnson <zakary.johnson575@gmail.com>
2024-07-26 20:40:30 -0500

    fixed the code to get the light mode and dark mode to work

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,17 @@
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('.container');
+const themeSwitcher = document.querySelector("#theme-switcher");
+const container = document.querySelector(".container");
 
-let mode = 'dark'
+let mode = "dark";
 
-themeSwitcher.addEventListener('click', function () {
-    // If mode is dark, apply light background
-    if (mode === 'dark') {
-      mode = 'light';
-      container.setAttribute('class', 'light');
-    }
-    // If mode is light, apply dark background
-    else {
-      mode = 'dark';
-      container.setAttribute('class', 'dark');
-    }
-  });
+themeSwitcher.addEventListener("click", function () {
+  // If mode is dark, apply light background
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+});
