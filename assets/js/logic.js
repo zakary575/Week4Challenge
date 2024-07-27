84e2887216090b266aca3e61860cfc763bb709d7
Zakary Johnson <zakary.johnson575@gmail.com>
2024-07-26 21:16:57 -0500

    used local storage to make light mode dark mode to persist through the page loading

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,30 @@
 const themeSwitcher = document.querySelector("#theme-switcher");
 const container = document.querySelector(".container");
-
-let mode = "dark";
+let mode = localStorage.getItem('mode')
+function init(){
+    // let mode = localStorage.getItem('mode')
+    if (mode === 'dark'){
+        container.setAttribute('class','dark')
+    }else if(mode === 'light'){
+        container.setAttribute('class','light')
+    }else{
+        
+    }return 
+}
 
 themeSwitcher.addEventListener("click", function () {
   // If mode is dark, apply light background
   if (mode === "dark") {
     mode = "light";
     container.setAttribute("class", "light");
+    localStorage.setItem('mode','light')
   }
   // If mode is light, apply dark background
   else {
     mode = "dark";
     container.setAttribute("class", "dark");
+    localStorage.setItem('mode','dark')
   }
 });
+
+init()
